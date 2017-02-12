a416608097b7d83f5bdeb343307ee30c53ac397f
Michael Yatco <mjyatco@gmail.com>
2017-02-12 11:17:43 -0500

    Create, update, and destroy mroutes created

--- app/views/students/index.json.jbuilder
+++ app/views/students/index.json.jbuilder
@@ -1,9 +1,1 @@
-json.array! @students.each do |student|
-  json.id student.id
-  json.first_name student.first_name
-  json.last_name student.last_name
-  json.email student.email
-  json.birthdate student.birthdate
-  json.university student.university
-  json.pokemon student.pokemon
-end
+json.array! @students, :id, :first_name, :last_name, :email, :birthdate, :university, :pokemon
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "students#index"
 
   get "/students", to: "students#index"
+  post "/students", to: "students#create"
   get "/students/:id", to: "students#show"
+  patch "/students/:id", to: "students#update"
+  delete "/students/:id", to: "students#destroy"
 
 end
