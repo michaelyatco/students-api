e5ecf957e44ff8e8342a1d8d4a64495b311940f3
Michael Yatco <mjyatco@gmail.com>
2017-02-14 21:28:35 -0500

    DRYed up controller and models

--- app/views/api/v2/students/_student.json.jbuilder
+++ app/views/api/v2/students/_student.json.jbuilder
@@ -4,6 +4,8 @@ if student.full_name.starts_with?("M")
 else
   json.full_name student.full_name
 end
+json.first_name student.first_name
+json.last_name student.last_name
 json.email student.email
 json.birthdate student.birthdate
 json.university student.university
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170212171155) do
+ActiveRecord::Schema.define(version: 20170214231911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,13 @@ ActiveRecord::Schema.define(version: 20170212171155) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "email"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "api_key"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
